31d97b4d9f3aae9fb2ea2ce0ad0a38e9aab578c8
daohainam <daohainam@gmail.com>
2024-12-16 11:12:41 +0100

    Clean up projects

--- ajax-data-table-models/obj/Debug/netstandard2.0/ajax-data-table-models.AssemblyInfo.cs
+++ ajax-data-table-models/obj/Debug/netstandard2.0/ajax-data-table-models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ajax-data-table")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60cf29215722f155d4250aa3bc89b75991e95277")]
 [assembly: System.Reflection.AssemblyProductAttribute("ajax-data-table")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ajax-data-table")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
